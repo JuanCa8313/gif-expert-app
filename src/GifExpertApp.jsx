2ea9d1488d7bc31b8f82418e0e1e7d2efbe9da21
Juan Camilo Arcila <juanca.arcilav@gmail.com>
2023-03-13 06:55:04 -0500

    Primer Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    // console.log(newCategory);
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+    // setCategories((cat) => ["Valorant", ...cat]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category}></GifGrid>
+      ))}
+    </>
+  );
+};
